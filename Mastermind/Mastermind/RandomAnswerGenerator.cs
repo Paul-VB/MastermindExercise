8412e348efcc62f4d3572b826cfddb98b91ed90c
Paul Vanden Broeck <PVB96P@gmail.com>
2026-07-28 16:30:40 -0400

    adding randomAnswerGenerator

---
+++ Mastermind/Mastermind/RandomAnswerGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    public class RandomAnswerGenerator
+    {
+        private const int answerLength = 4;
+        private readonly char[] possibleChars = ['1', '2', '3', '4', '5', '6'];
+        public string Generate()
+        {
+            var random = new Random();
+
+            var newAnswer = new StringBuilder();
+            for (int i = 0; i < answerLength; i++)
+            {
+                var newAnswerChar = possibleChars[random.Next(0, possibleChars.Length)];
+                newAnswer.Append(newAnswerChar);
+            }
+
+            return newAnswer.ToString();
+        }
+    }
+}
