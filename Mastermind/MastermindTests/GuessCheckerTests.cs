439fb79de9d65625c48037dc17bdfe0cbf020257
Paul Vanden Broeck <PVB96P@gmail.com>
2026-07-28 18:54:22 -0400

    additional work on unit tests

--- Mastermind/MastermindTests/GuessCheckerTests.cs
+++ Mastermind/MastermindTests/GuessCheckerTests.cs
@@ -1,14 +1,26 @@
-﻿using Xunit;
+﻿using Mastermind;
+using Xunit;
 
 namespace MastermindTests
 {
-    [TestClass]
     public class GuessCheckerTests
     {
+        private readonly GuessChecker checker;
+        public GuessCheckerTests()
+        {
+            checker = new GuessChecker();
+        }
 
         [Fact]
         public void TestMethod1()
         {
+            // Arrange
+            string userGuess = "1234";
+            string actualAnswer = "1234";
+            // Act
+            string result = checker.CheckGuess(userGuess, actualAnswer);
+            // Assert
+            Assert.Equal("++++", result);
         }
     }
 }
